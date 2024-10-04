0dc17fda93720c8c52a869ee92ea90327f9ff0cd
Benjamin Ng <benjaminngyt@gmail.com>
2024-10-04 12:18:02 +0800

    Initialize Project

---
+++ shoppingcart/src/test/java/com/shoppingcart/springboot/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingcart.springboot.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
