d1eef469d5c8a3cf31ed0d0abde6e7e938bafd85
pochine95 <pochine95@gmail.com>
2024-10-08 11:58:36 +0800

    updated

--- src/main/java/com/shoppingcart/springboot/ShoppingCartApplication.java
+++ src/main/java/com/shoppingcart/springboot/ShoppingCartApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingCartApplication {
 
     public static void main(String[] args) {
+        System.out.print("hg");
         SpringApplication.run(ShoppingCartApplication.class, args);
     }
 
