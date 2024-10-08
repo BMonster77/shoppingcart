e696e86f1aa6ff4447e6145b878110cc679ada0d
ZHOU PING <zhouping151140@gmail.com>
2024-10-08 22:49:36 +0800

    Add files via upload

--- src/main/java/com/shoppingcart/springboot/repository/ProductRepository.java
+++ src/main/java/com/shoppingcart/springboot/repository/ProductRepository.java
@@ -2,12 +2,15 @@ package com.shoppingcart.springboot.repository;
 
 import com.shoppingcart.springboot.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
 
-import java.util.ArrayList;
+
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
-
+    List<Product> findByNameContaining(String keyword, Pageable pageable);  // 支持分页的模糊搜索
+    Page<Product> findAll(Pageable pageable);  // 分页获取所有产品
 }
 
