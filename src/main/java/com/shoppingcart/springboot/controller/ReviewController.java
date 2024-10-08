e7acc25de59b38e27b9a4da5ccaeb393078d0dc9
pochine95 <pochine95@gmail.com>
2024-10-08 12:16:39 +0800

    update review controller

--- src/main/java/com/shoppingcart/springboot/controller/ReviewController.java
+++ src/main/java/com/shoppingcart/springboot/controller/ReviewController.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Controller;
 public class ReviewController {
 
 
-//    @PostMapping("/products/{productId}/review")
-//    public String leaveReview(@PathVariable Long productId, @RequestParam Long userId,
-//                              @RequestBody Review review) {
-//        Product product = productService.getProductById(productId);
-//        User user = userService.getUserById(userId);
-//        if (product != null && user != null) {
-//            reviewService.addReview(product, user, review);
-//            return "Review added successfully!";
-//        }
-//        return "Failed to add review.";
-//    }
+    @PostMapping("/products/{productId}/review")
+    public String leaveReview(@PathVariable Long productId, @RequestParam Long userId,
+                              @RequestBody Review review) {
+        Product product = productService.getProductById(productId);
+        User user = userService.getUserById(userId);
+        if (product != null && user != null) {
+            reviewService.addReview(product, user, review);
+            return "Review added successfully!";
+        }
+        return "Failed to add review.";
+    }
 }
