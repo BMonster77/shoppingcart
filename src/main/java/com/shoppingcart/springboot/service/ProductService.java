61dffd7513e5ff03aff947764a4012654e18c2d7
Feb2uu <2583746376@qq.com>
2024-10-08 17:31:31 +0800

    Finish basic ProductController

--- src/main/java/com/shoppingcart/springboot/service/ProductService.java
+++ src/main/java/com/shoppingcart/springboot/service/ProductService.java
@@ -1,5 +1,10 @@
 package com.shoppingcart.springboot.service;
 
-public interface ProductService {
+import java.util.List;
 
+import com.shoppingcart.springboot.model.Product;
+
+public interface ProductService {
+    List<Product> getProductsByPage(int page, int size);
+    Product getProductById(Long productId);
 }
